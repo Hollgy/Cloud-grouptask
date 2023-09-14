fa852d90932286d03043b66e87e74e9be9ba35a1
KG <karlgustav.ek@hotmail.com>
2023-09-14 12:14:10 +0200

    checkpoint, can view imgs on site, paircode, no folders atm

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import AWS from 'aws-sdk';
-import { useEffect } from 'react';
 
 AWS.config.update({
     accessKeyId: '',
@@ -12,39 +11,40 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 function App() {
-    const [selectedFile, setSelectedFile] = useState([])
-    const [images, setImages] = useState(null)
+    const [selectedFile, setSelectedFile] = useState([]);
+    const [images, setImages] = useState([]);
 
     useEffect(() => {
-        getImages()
+        getImages();
     }, []);
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
 
-    const handleUpload = () => {
-        if (!selectedFile) {
-            alert('Välj en fil först.');
-            return;
-        }
+const handleUpload = () => {
+    if (!selectedFile) {
+        alert('Välj en fil först.');
+        return;
+    }
 
-        const params = {
-            Bucket: 'gruppfunktion',
-            Key: selectedFile.name,
-            Body: selectedFile,
-        };
-
-        s3.upload(params, (err, data) => {
-            if (err) {
-                console.error('Fel vid uppladdning:', err);
-            } else {
-                console.log('Bilden laddades upp!:', data.Location);
-
-                setImages((prevImages) => [...prevImages, selectedFile.name])
-            }
-        });
+    const params = {
+        Bucket: 'gruppfunktion',
+        Key: selectedFile.name,
+        Body: selectedFile,
+        ContentType: selectedFile.type,
+        ACL: 'public-read', // Set ACL to public-read to make the file accessible
     };
+
+    s3.upload(params, (err, data) => {
+        if (err) {
+            console.error('Fel vid uppladdning:', err);
+        } else {
+            console.log('Bilden laddades upp!:', data.Location);
+            getImages(); // Refresh the list of images after upload
+        }
+    });
+};
 
     const getImages = () => {
         const params = {
@@ -55,7 +55,6 @@ function App() {
             if (err) {
                 console.error('Fel vid hämtning av bilder:', err);
             } else {
-
                 const imageList = data.Contents.map((obj) => obj.Key);
                 setImages(imageList);
             }
@@ -69,8 +68,15 @@ function App() {
                 <input onChange={handleFileChange} type="file" />
                 <button onClick={handleUpload}>Ladda Upp</button>
             </div>
+            <h2>Bilder i S3-bucket:</h2>
+            <ul>
+                {images.map((image, index) => (
+                    <li key={image}><img src={`https://gruppfunktion.s3.eu-north-1.amazonaws.com/${image}`} alt={`Image ${index}`} /></li>
+
+                ))}
+            </ul>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
